63258235c2d44701d4a27db82af2ea76b2cb2e46
PetitJump <killianbonnin7@gmail.com>
2025-11-21 17:02:17 +0100

    Ajout de la structure de base

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
-void acheter_fruit(){
+void ajouter_fruit(){
 
 }
 
@@ -9,10 +10,24 @@ void vendre_fruit(){
 }
 
 void afficher_stock(){
-    
+
 }
 
 int main(){
+    char reponse[] = "";
+    while (strcmp(reponse, "quitter") != 0){ // Tant que la réponse n'est pas "quitter"
+        afficher_stock();
 
+        printf("\nQue voulez vous faire ? (vendre/ajouter/quitter) : ");
+        scanf("%s", reponse);
+
+        if (strcmp(reponse, "vendre") == 0){
+            vendre_fruit();
+        }
+
+        else if (strcmp(reponse, "ajouter") == 0){
+            ajouter_fruit();
+        }
+    }
     return 0;
 }
